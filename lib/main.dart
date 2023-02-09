95124e51cee912b1f089cdd9ffa9d0d7421340c8
Angel Jaramillo <angeljara07@outlook.com>
2023-02-08 23:33:45 -0500

    agregando imagen al primer diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:disenos/screens/basic_design.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,10 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Center(
-        child: Text("Hola Mundo"),
-      ),
+      initialRoute: "basic_design",
+      routes: {
+        "basic_design": (_) => const BasicDesignScreen()
+      },
     );
   }
 }
